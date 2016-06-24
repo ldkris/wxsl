a30734c780ec17a1ee8bbc6ae6ae7b57c7e8b61c
刘冬 <576245568@qq.com>
2016-06-24 10:02:17 +0800

    Initial Commit

---
+++ wxsl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wxsl
+//
+//  Created by 刘冬 on 16/6/24.
+//  Copyright © 2016年 刘冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
